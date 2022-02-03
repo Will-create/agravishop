3d5a6094b286bbdf995b3a78a33d5bcae518dcc6
Louis Bertson <louisbertson@gmail.com>
2022-02-03 13:56:15 +0000

    first commit

---
+++ release.js
@@ -0,0 +1,12 @@
+// ===================================================
+// FOR PRODUCTION
+// Total.js - framework for Node.js platform
+// https://www.totaljs.com
+// ===================================================
+
+var fs = require('fs');
+var options = {};
+options.ip = 'auto';
+options.port = process.env.PORT;
+var framework = require('total.js');
+framework.http('release', options);
